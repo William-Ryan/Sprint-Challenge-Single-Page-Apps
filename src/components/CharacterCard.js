82d35c2589f432377f4bc34a472b2680de81b485
William Ryan <billy.ryan.rom@gmail.com>
2020-01-31 12:52:24 -0500

    Add Search to Character List

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function CharacterCard(props) {
   return (
     <div className="character-card">
-      <img src={props.character.image} />
+      <img src={props.character.image} alt="Rick and Morty Character"/>
       <h2>Name: {props.character.name}</h2>
       <p>Gender: {props.character.gender}</p>
       <h3>Species: {props.character.species}</h3>
