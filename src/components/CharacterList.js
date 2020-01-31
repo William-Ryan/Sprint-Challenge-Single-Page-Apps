7c33f5473d667120edf67b1083755d94064e2823
William Ryan <billy.ryan.rom@gmail.com>
2020-01-31 11:35:47 -0500

    Add useState for Data/Add Axios Get to CharacterList.js

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,9 +1,25 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Axios from "axios";
+import CharacterCard from "./CharacterCard";
+import SearchForm from "./SearchForm";
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  
+  const [characters, setCharacters] = useState([]);
+  const [filteredData, updateData] = useState([])
+
 
   useEffect(() => {
+    Axios.get("https://rickandmortyapi.com/api/character/")
+    .then(res => {
+      console.log(res.data.results);
+      setCharacters(res.data.results);
+      updateData(res.data.results);
+    })
+    .catch(err => {
+      console.log("Not your data", err);
+    })
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
   }, []);
