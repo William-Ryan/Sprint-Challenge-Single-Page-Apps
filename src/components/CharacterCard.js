d3bfd0d2515136ac22993e72649d55dc210676e8
William Ryan <billy.ryan.rom@gmail.com>
2020-01-31 12:05:30 -0500

    Render Character Elements to Character Page

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return (
+    <div className="character-card">
+      <img src={props.character.image} />
+      <h2>Name: {props.character.name}</h2>
+      <p>Gender: {props.character.gender}</p>
+      <h3>Species: {props.character.species}</h3>
+      <p>Origin: {props.character.origin.name}</p>
+      <p>Status: {props.character.status}</p>
+    </div>
+  )
 }
